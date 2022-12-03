da4e2f7260983d8ae52f0d46fe0af151f0fabda8
Bastien Flanquart <44155367+AwiZy63@users.noreply.github.com>
2022-12-03 15:24:42 +0100

    Update 003-ports.js
    Foreign key contraint was incorrectly formed

--- api/migrations/003-ports.js
+++ api/migrations/003-ports.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Servers',
+          model: 'servers',
           key: 'id',
           onUpdate: 'CASCADE',
           onDelete: 'CASCADE'
